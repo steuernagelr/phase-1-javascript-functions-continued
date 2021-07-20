23e599a3d2d93a6c362875dcf7fe1e18b65614bb
Rielly Steuernagel <reiguy@comcast.net>
2021-07-20 11:53:05 -0400

    commit tree 269e06151a52026691fa68289e2f5348d86c4538

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work='go to the office'){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(){
+    return function(attribute='special', flair="*"){
+        return `You are ${flair}${attribute}${flair}!`
+    }
+}
+
+wrapAdjective("a dedicated programmer", "||")
